b27a386186ae5cbaf2846d88780881dbaa1c4214
WHcoder351 <76533189+WHcoder351@users.noreply.github.com>
2021-03-16 20:39:30 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var pizzas = ["Pepperoni","Cheese","Veg","Margherita","Hawaiian","Supreme(Veg)", "Supreme(Meat)"];
+function getmenu(){
+var Hdat;
+Hdat = "<ol class = 'menulist'>"
+pizzas.sort();
+    for(var i = 0;i<pizzas.length;i++){
+        Hdat=Hdat+ '<li>' + pizzas[i] + '</li>'
+    }
+    Hdat=Hdat+"</ol>"
+    document.getElementById("menu_list").innerHTML = Hdat;
+}
+function add_item(){
+    Hdat;
+    item = document.getElementById("add_item").value;
+    pizzas.push(item);
+    pizzas.sort()
+    Hdat = "<section class = 'cards'> "
+    for(var i=0;i<pizzas;i++){
+        Hdat = Hdat+"<div class = 'card' >" + "<img src = 'images/pizzaImg.png/>' " + pizzas[i]+"</div>" 
+    }
+    Hdat = Hdat+"</section>"
+    document.getElementById("display_addmenu").innerHTML = Hdat;
+}
